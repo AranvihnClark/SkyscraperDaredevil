2b03773ddbd61d6934cc1f924cf19637a034d23d
Aranvihn Clark <aranvihn.clark@gmail.com>
2023-10-06 16:27:20 -0400

    Global Variables initial Commit

---
+++ Assets/Scripts/GameData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Schema;
+using UnityEngine;
+
+public static class GameData
+{
+    // Global Game Data
+    public static int currentLevel = 0;
+
+    public static int totalDeaths = 0;
+}
---
+++ Assets/Scripts/LevelData.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Schema;
+using UnityEngine;
+
+public class LevelData
+{
+    private int level;
+    private int time;
+    private int items;
+    private int score;
+    private int total;
+    private int deaths;
+
+    // Empty constructor used when starting a level (aside from Level 1) to create new data as needed to save on space.
+    public LevelData() 
+    {
+        // Increase our game data's current level by 1.
+        GameData.currentLevel++;
+    }
+
+    // Full constructor to be updated at the end of a level.
+    public LevelData(int time, int items, int score, int total, int deaths)
+    {
+        level = GameData.currentLevel;
+        this.time = time;
+        this.items = items;
+        this.score = score;
+        this.total = total;
+        this.deaths = deaths;
+    }
+}
