8037d213413cb3f5ba2ec63bc10a6c69c3ddbdbc
Aranvihn Clark <aranvihn.clark@gmail.com>
2023-10-10 16:37:01 -0400

    Changed Name Due to  C# duplicates

--- Assets/Scripts/TimerSet.cs
+++ Assets/Scripts/TimerSet.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Timer : MonoBehaviour
+public class TimerSet : MonoBehaviour
 {
     [SerializeField] GameObject player;
     public float timeLeft = 60;
-    public bool timerOn = false;
+    public static bool timerOn = false;
 
     // Start is called before the first frame update
     private void Start()
