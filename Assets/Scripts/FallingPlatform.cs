e5cfd547db927aba73a98763c0dc529b850fbb14
Aranvihn Clark <aranvihn.clark@gmail.com>
2023-10-23 14:59:04 -0400

    Update - added falling steel beams

---
+++ Assets/Scripts/FallingPlatform.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlatform : MonoBehaviour
+{
+    
+    private SpriteRenderer sprite;
+    private Rigidbody2D rg;
+    private float alphaTime = 0.8f;
+    private bool isCollided = false;
+
+    public bool isTriggered = false;
+
+    private void Start()
+     {
+        sprite = gameObject.GetComponent<SpriteRenderer>();
+        rg = gameObject.GetComponent<Rigidbody2D>();
+     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isTriggered = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (isTriggered)
+        {
+            if (alphaTime > 0f)
+            {
+                alphaTime -= Time.deltaTime;
+            }
+            else 
+            {
+                rg.bodyType = RigidbodyType2D.Dynamic;
+            }
+        }
+        if (isTriggered && isCollided)
+        {
+            sprite.color = new Color (sprite.color.r, sprite.color.g, sprite.color.b, sprite.color.a - (2.5f * Time.deltaTime));
+            if (sprite.color.a <= 0f)
+            {
+                PlatformVanish();
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player")) {}
+        else if (collision.gameObject.CompareTag("FallThrough")) {}
+        else
+        {
+            isCollided = true;
+        }
+    }
+
+    private void PlatformVanish()
+    {
+        Destroy(gameObject);
+    }
+
+}
