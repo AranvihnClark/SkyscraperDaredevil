786fe42600b62befae2cd0eab2334baa8d00f2de
Aranvihn Clark <aranvihn.clark@gmail.com>
2023-10-30 13:34:22 -0400

    Update - renamed start menu

---
+++ Assets/Scripts/GameMenu.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        // Initializing a level's data by creating the 'level' object itself.
+        GameData.Begin();
+
+        // Moves the scene to the next scene in the build index.
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void OptionsMenu()
+    {
+
+    }
+
+    public void Credits()
+    {
+        
+    }
+
+    public void Exit()
+    {
+        // Simple method to 'quit' the game.
+        Application.Quit();
+    }
+}
