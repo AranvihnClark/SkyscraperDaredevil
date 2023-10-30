cc9a0759a1e2fd51dbd958ce7773bb3d914e544f
Aranvihn Clark <aranvihn.clark@gmail.com>
2023-10-30 13:36:20 -0400

    Update - Forgot to update class name

--- Assets/Scripts/GameMenu.cs
+++ Assets/Scripts/GameMenu.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class StartMenu : MonoBehaviour
+public class GameMenu : MonoBehaviour
 {
     public void StartGame()
     {
