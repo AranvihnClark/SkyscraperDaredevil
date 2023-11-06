f190c96fcfd9259bd254748a4a0ce8f4295284a1
Aranvihn Clark <aranvihn.clark@gmail.com>
2023-11-06 14:14:54 -0500

    Update - Remove debug.log

--- Assets/Scripts/AnimatingComplete.cs
+++ Assets/Scripts/AnimatingComplete.cs
@@ -47,7 +47,6 @@ public class AnimatingComplete : MonoBehaviour
 
     private void StartTime()
     {
-        Debug.Log("HELLO!!!???");
         TimerSet.timerOn = true;
         timeCount++;
     }
